caa6fd930f645fc53482a9fdd9e19be15eac1f02
Edin Jašarević <edinjasar14@gmail.com>
2020-11-09 12:43:27 +0100

    the demo sample importing countries

---
+++ TopTenPops/TopTenPops/CsvReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TopTenPops
+{
+    class CsvReader
+    {
+        private string _csvFilePath;
+
+        public CsvReader(string csvFilePath)
+        {
+            this._csvFilePath = csvFilePath;
+        }
+
+        public Country[] ReadFirstNCountries(int nCountries)
+        {
+            return null;
+        }
+    }
+}
---
+++ TopTenPops/TopTenPops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TopTenPops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "Pop by Largest Final.csv";
+
+            CsvReader reader = new CsvReader(filePath);
+
+            Country[] countries = reader.ReadFirstNCountries(10);
+
+            foreach (var country in countries)
+            {
+                Console.WriteLine($"{country.Population}: {country.Name}");
+            }
+        }
+    }
+}
