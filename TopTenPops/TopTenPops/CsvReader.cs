a75d674ce945a4e56c6db05c4b676726b2fce25b
Edin Jašarević <edinjasar14@gmail.com>
2020-11-09 22:12:46 +0100

    parsing a csv line wiht string split

--- TopTenPops/TopTenPops/CsvReader.cs
+++ TopTenPops/TopTenPops/CsvReader.cs
@@ -19,5 +19,17 @@ namespace TopTenPops
             Country[] countries = new Country[nCountries];
             return countries;
         }
+
+        public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(',');
+
+            string name = parts[0];
+            string code = parts[1];
+            string region = parts[2];
+            int population = int.Parse((parts[3]));
+
+            return new Country(name, code, region, population);
+        }
     }
 }
