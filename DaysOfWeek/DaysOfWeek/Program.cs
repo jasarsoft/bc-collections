0452faa60b3e1abf0d463472a16bcf2c78e9cfaf
Edin Jašarević <edinjasar14@gmail.com>
2020-11-09 11:56:22 +0100

    looking up array items

--- DaysOfWeek/DaysOfWeek/Program.cs
+++ DaysOfWeek/DaysOfWeek/Program.cs
@@ -23,6 +23,13 @@ namespace DaysOfWeek
             {
                 Console.WriteLine(day);
             }
+
+            Console.WriteLine("Which day do you want to display?");
+            Console.Write("(Monday = 1, etc) > ");
+            int iDay = int.Parse(Console.ReadLine());
+
+            string chosenDay = daysOfWeek[iDay];
+            Console.WriteLine($"That day is {chosenDay}");
         }
     }
 }
