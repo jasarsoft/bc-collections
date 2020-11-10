057e4a84a1722c8b84bc8bb93693bda8b4c8419f
Edin Jašarević <edinjasar14@gmail.com>
2020-11-10 23:23:15 +0100

    introducing list

---
+++ DaysOfWeekList/DaysOfWeekList/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DaysOfWeekList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> daysOfWeek = new List<string>();
+
+            List<int> ints = new List<int>();
+        }
+    }
+}
