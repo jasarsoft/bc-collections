55897302198cef35af2f3e240a64d597c38c8ee0
Edin Jašarević <edinjasar14@gmail.com>
2020-11-10 23:49:45 +0100

    counting element with array length

--- TopTenPops/TopTenPops/CsvReader.cs
+++ TopTenPops/TopTenPops/CsvReader.cs
@@ -54,13 +54,32 @@ namespace TopTenPops
 
         public Country ReadCountryFromCsvLine(string csvLine)
         {
-            string[] parts = csvLine.Split(new char[]{','});
+            string[] parts = csvLine.Split(',');
+            string name;
+            string code;
+            string region;
+            string popText;
+            switch (parts.Length)
+            {
+                case 4:
+                    name = parts[0];
+                    code = parts[1];
+                    region = parts[2];
+                    popText = parts[3];
+                    break;
+                case 5:
+                    name = parts[0] + ", " + parts[1];
+                    name = name.Replace("\"", null).Trim();
+                    code = parts[2];
+                    region = parts[3];
+                    popText = parts[4];
+                    break;
+                default:
+                    throw new Exception($"Can't parse country from csvLine: {csvLine}");
+            }
 
-            string name = parts[0];
-            string code = parts[1];
-            string region = parts[2];
-            int population = int.Parse((parts[3]));
-
+            //tryparse leacese population = 0 if can't parse
+            int.TryParse(popText, out int population);
             return new Country(name, code, region, population);
         }
     }
