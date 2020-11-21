26cc3400a65486fe0c6d209d065f02ddd0f540b5
Edin Jašarević <edinjasar14@gmail.com>
2020-11-21 16:53:06 +0100

    limiting how many results with take

---
+++ TopTenPops/ReadAllCountries/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TopTenPops;
+
+namespace ReadAllCountries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "../../../../Pop by Largest Final.csv";
+            CsvReader reader = new CsvReader(filePath);
+            List<Country> countries = reader.ReadAllCountries();
+
+            foreach (var country in countries.Take(10))
+            {
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
+        }
+    }
+}
