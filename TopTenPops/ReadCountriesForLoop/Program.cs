f1714911a3b544eb1855cc5ab758d8d6ebe22a33
Edin Jašarević <edinjasar14@gmail.com>
2020-11-21 15:07:11 +0100

    iterating only some items

--- TopTenPops/ReadCountriesForLoop/Program.cs
+++ TopTenPops/ReadCountriesForLoop/Program.cs
@@ -10,10 +10,18 @@ namespace ReadCountriesForLoop
         {
             string filePath = "../../../../Pop by Largest Final.csv";
             CsvReader reader = new CsvReader(filePath);
-
             List<Country> countries = reader.ReadAllCountries();
 
-            for (int i = 0; i < countries.Count; i++)
+            Console.Write("Enter no. of countries to display> ");
+            bool inputIsInt = int.TryParse(Console.ReadLine(), out int userInput);
+            if (!inputIsInt || userInput <= 0)
+            {
+                Console.WriteLine("You must type in a integer. Exiting");
+                return;
+            }
+
+            int maxToDisplay = Math.Min(userInput, countries.Count);
+            for (int i = 0; i < maxToDisplay; i++)
             {
                 var country = countries[i];
                 Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
